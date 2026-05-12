44286cfa304cac702e396cc8e0fd9d6e7976f43b
Nguyen Minh Tri <tri21723@gmail.com>
2026-05-12 21:36:49 +0700

    test: thay doi banner de test pipeline

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -10,7 +10,7 @@ const Banner = ({ appName, token }) => {
         <h1 className="logo-font">
           {appName.toLowerCase()}
         </h1>
-        <p>A place to share your knowledge.</p>
+        <p>Hệ thống CI/CD tự động hóa của Nguyễn Minh Trí!</p>
       </div>
     </div>
   );
